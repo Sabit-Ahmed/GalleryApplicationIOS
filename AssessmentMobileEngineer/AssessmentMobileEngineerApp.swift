df2c725a92c9ab27ac72609b9e018bcc31ede7f4
Sabit Ahmed <sabit.a.sirat@gmail.com>
2022-05-16 15:20:31 +0600

    Initial Commit

---
+++ AssessmentMobileEngineer/AssessmentMobileEngineerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentMobileEngineerApp.swift
+//  AssessmentMobileEngineer
+//
+//  Created by Sabit Ahmed on 16/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentMobileEngineerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
